ea9498c440388dcf328e3fe20bd5beeeaeaf8624
efl93 <eftl93@gmail.com>
2022-02-20 09:55:23 -0600

    First changes
    added source file, image for reference,
    and modify the readme file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # seven_seg_sv
- 7-segment module in SystemVerilog
+This module is to create a decoder for 7-segment displays.
+The input consists of four bits which will reprecent a binary number (0x00-0x0F)
+This module was written mainly for Common Anode 7-segment - Applying a logic '0' to a segment will shut it down. 
+The LSB is is segment 'a' and the MSB is segment 'g'
---
+++ seven_segment.sv
@@ -0,0 +1,34 @@
+module seven_segment(
+					input[3:0]in, 
+					output[6:0]out
+);
+
+wire [3:0]number_in;
+reg [6:0]segment_out;
+
+assign number_in[3:0] = in[3:0];
+assign out[6:0] = segment_out[6:0];
+
+always @(*)
+begin
+	case(number_in)
+		4'h0: segment_out = 7'b1000000;
+		4'h1: segment_out = 7'b1111001;
+		4'h2: segment_out = 7'b0100100;
+		4'h3: segment_out = 7'b0110000;
+		4'h4: segment_out = 7'b0011001;
+		4'h5: segment_out = 7'b0010010;
+		4'h6: segment_out = 7'b0000010;
+		4'h7: segment_out = 7'b1111000;
+		4'h8: segment_out = 7'b0000000;
+		4'h9: segment_out = 7'b0010000;
+		4'ha: segment_out = 7'b0001000;
+		4'hb: segment_out = 7'b0000011;
+		4'hc: segment_out = 7'b1000110;
+		4'hd: segment_out = 7'b0100001;
+		4'he: segment_out = 7'b0000110;
+		4'hf: segment_out = 7'b0001110;
+		default:	segment_out = 7'b1111111;
+	endcase
+end
+endmodule
